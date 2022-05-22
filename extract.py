56aeba3c724add72d52ab5160e0890258a7f0728
redphx <96280+redphx@users.noreply.github.com>
2022-05-22 17:14:45 +0700

    Get MAC address

--- extract.py
+++ extract.py
@@ -38,6 +38,15 @@ for tuya_device in device_manager.device_map.values():
         'local_key': tuya_device.local_key,
     }
 
+    try:
+        resp = openapi.get('/v1.0/iot-03/devices/factory-infos?device_ids={}'.format(tuya_device.id))
+        factory_info = resp['result'][0]
+        if 'mac' in factory_info:
+            mac = ':'.join(factory_info['mac'][i:i + 2] for i in range(0, 12, 2))
+            device['mac_address'] = mac
+    except Exception:
+        pass
+
     devices.append(device)
 
 print(json.dumps(devices, indent=2))
